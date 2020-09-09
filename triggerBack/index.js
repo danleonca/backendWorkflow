6d1529ec625d00686c39ae2c06add180f1983d01
shari12 <shariperez97@gmail.com>
2020-09-08 23:12:57 -0500

    conditions for messages return chatbot

--- triggerBack/index.js
+++ triggerBack/index.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const request = require('sync-request');
 const app = express();
 app.use(bodyParser.json());
-let sToken;
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
 
 app.post('/testChatbot', function (req, res) {
     let getInfo = req.body.conversation.memory;
     let person = getInfo.person.raw;
     let department = getInfo.department.raw;
-    let devicepurchase = getInfo.devicepurchase.raw;
-    let devicereserve = getInfo.devicereserve.raw;
+    let devicepurchase = getInfo.devicepurchase ? getInfo.devicepurchase.raw : undefined;
+    let devicereserve = getInfo.devicereserve ? getInfo.devicereserve.raw : undefined;
     let email = getInfo.email.raw;
     let aContext = [];
+    let rContext = [];
+    let data = {
+        replies : []
+    };
+    let sBolean = true;
+    const oUser = {
+        "userName":person,
+        "email":email,
+        "department": department
+    };
 
     if (devicepurchase) {
         let device;
@@ -51,7 +56,7 @@ app.post('/testChatbot', function (req, res) {
                     aContext.push(product);
                 }
                 break;
-            case "celulares":
+            case "Celular":
                 device = getInfo.celulares.raw;
                 if (device === "Apple - iPhone XS - 512 GB") {
                     const product = {
@@ -61,9 +66,7 @@ app.post('/testChatbot', function (req, res) {
                         "image": "https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000224778_sd.jpg"
                     }
                     aContext.push(product);
-
                 }
-
                 if (device === "Apple - iPhone 11 Pro 256 GB") {
                     const product = {
                         "name": "Apple - iPhone 11 Pro 256 GB",
@@ -83,7 +86,7 @@ app.post('/testChatbot', function (req, res) {
                     aContext.push(product);
                 }
                 break;
-            case "tablet":
+            case "Tableta":
                 device = getInfo.tablet.raw;
                 if (device === "Apple - iPad 7 + 32 GB - Oro") {
                     const product = {
@@ -114,7 +117,6 @@ app.post('/testChatbot', function (req, res) {
                 }
                 break;
         }
-
     }
     if (devicereserve) {
         let deviceR;
@@ -127,7 +129,7 @@ app.post('/testChatbot', function (req, res) {
                         "request": "Reserva de equipo",
                         "image": "https://www.officedepot.com.mx/medias/68472.png-1200ftw?context=bWFzdGVyfHJvb3R8MTE5MTIyNnxpbWFnZS9wbmd8aDAzL2gyMi85NzIzMDI5ODQ4MDk0LnBuZ3xjYmIwOTQ4ZWMwZjkwOTI0NWViNjI2YzNmOTBjNzQwZDc4ZDU1ZWIxNjg4NTA0OWUwZDIwZWRhYzBjMjMzZmU2"
                     }
-                    aContext.push(reserva);
+                    rContext.push(reserva);
                 }
                 if (deviceR === "Silla Ejecutiva Office Depot Nápoles / Malla / Negro") {
                     const reserva = {
@@ -135,7 +137,7 @@ app.post('/testChatbot', function (req, res) {
                         "request": "Reserva de equipo",
                         "image": "https://www.officedepot.com.mx/medias/60148.png-1200ftw?context=bWFzdGVyfHJvb3R8OTgwMTc5fGltYWdlL3BuZ3xoMzUvaGJhLzk3MjMwMjY1NzEyOTQucG5nfGQyNjU4MWZiNDgwNDNlZmFhZTZhNGFiN2NlM2U4NWRjYTM3ODI5ZDFiZDRhNTRhYzU3NDhhZThkNzU4ZTNlZTA"
                     }
-                    aContext.push(reserva);
+                    rContext.push(reserva);
                 }
                 if (deviceR === "Silla de Oficina Red Top Power / Malla / Negro") {
                     const reserva = {
@@ -143,7 +145,7 @@ app.post('/testChatbot', function (req, res) {
                         "request": "Reserva de equipo",
                         "image": "https://www.officedepot.com.mx/medias/68433.gif-1200ftw?context=bWFzdGVyfHJvb3R8MTAyMTY2fGltYWdlL2pwZWd8aDgyL2hmMy85NDE2NjQ0OTE5MzI2LmpwZ3wzZGZhZTE2MTg2MGJmMzk4MmU5YmVlYjM4NWQ3YmFiYmMxNTllNzM2ODQ5YzZkZjQ5ZWZmNmNhY2IwN2ZlZGE1"
                     }
-                    aContext.push(reserva);
+                    rContext.push(reserva);
                 }
                 break;
             case "monitores":
@@ -154,7 +156,7 @@ app.post('/testChatbot', function (req, res) {
                         "request": "Reserva de equipo",
                         "image": "https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000230532cv11d.jpg"
                     }
-                    aContext.push(reserva);
+                    rContext.push(reserva);
                 }
                 if (deviceR === "LG - Monitor Full HD LED IPS de 2") {
                     const reserva = {
@@ -162,7 +164,7 @@ app.post('/testChatbot', function (req, res) {
                         "request": "Reserva de equipo",
                         "image": "https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000221775_sa.jpg"
                     }
-                    aContext.push(reserva);
+                    rContext.push(reserva);
                 }
                 if (deviceR === "Samsung - Monitor curvo de 34 QLED") {
                     const reserva = {
@@ -170,66 +172,147 @@ app.post('/testChatbot', function (req, res) {
                         "request": "Reserva de equipo",
                         "image": "https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000215325_sa.jpg"
                     }
-                    aContext.push(reserva);
+                    rContext.push(reserva);
                 }
         }
     }
 
 
-    console.log(aContext);
-    //getWorkflowAccessToken();
+    if(devicereserve){
+       let sType = "Reserva";
+       const oHeadersGet = { 'Authorization': "Basic c2ItY2xvbmUtZDhjMmJmNDgtYmNmZS00NzVhLThjN2MtZjZiZGU4YzI1MWRjIWIxMjYzM3x3b3JrZmxvdyFiMTc3NDo0N2RiODJkMS02OTM1LTQ0OTctYTkxZS1lNTFhNDVmMDExYjQkNGFsMzVBVkFrWnZLa2hzdE5kUWR5ZERWR0ptSkNHX0hKYWlXcXJGQlBmRT0=" }  // Basic Authorization in base 64, clientid is user and cliendsecret is the password
+       const oParamsGet = {
+           json: true,
+           headers: oHeadersGet
+       }
+        let resGet = request('POST', 'https://c9de1e9btrial.authentication.us10.hana.ondemand.com/oauth/token?grant_type=client_credentials', oParamsGet);
+        let bodyGet = JSON.parse(resGet.getBody('utf8'));
+       //console.log("bodyGet", bodyGet.access_token)
+       const oHeadersPost = { 
+            'Authorization': 'Bearer '+ bodyGet.access_token,
+            'Content-Type':'application/json'
+             }
+        let oContext = {
+                "definitionId": "testWorkflow",
+                "context": {
+                    "sType": sType,
+                    "users":oUser,
+                    "items":rContext
+                        }
+                    }
+       //const oHeadersPost = { 'Authorization': "Basic c2ItY2xvbmUtZDhjMmJmNDgtYmNmZS00NzVhLThjN2MtZjZiZGU4YzI1MWRjIWIxMjYzM3x3b3JrZmxvdyFiMTc3NDo0N2RiODJkMS02OTM1LTQ0OTctYTkxZS1lNTFhNDVmMDExYjQkNGFsMzVBVkFrWnZLa2hzdE5kUWR5ZERWR0ptSkNHX0hKYWlXcXJGQlBmRT0=" }  // Basic Authorization in base 64, clientid is user and cliendsecret is the password
+       const oParamsPost = {
+           json: oContext,
+           headers: oHeadersPost
+       }
 
-    triggerInstances(aContext);
+       try{
+        let resPost = request('POST', 'https://api.workflow-sap.cfapps.us10.hana.ondemand.com/workflow-service/rest/v1/workflow-instances', oParamsPost);
+        let bodyPost =JSON.parse(resPost.getBody('utf8'));
+        console.log("bodyPost ", bodyPost);
+       if(bodyPost.status==='RUNNING'){
+        data.replies.push({
+            "type": "text",
+            "content": "Se activó el flujo de aprobación para tu solicitud de reserva con éxito."
+        });
+       }else{
+        data.replies.push({
+            "type": "text",
+            "content": "Ha surgido un error al momento de activar el flujo de aprobación, por favor intente más tarde."
+        
+        });
+         sBolean = false;
+    }  
+        }catch(error){
+            data.replies.push({
+                "type": "text",
+                "content": "Ha surgido un error al momento de activar el flujo de aprobación, por favor intente más tarde."
+            }); 
+            sBolean = false;
+            }
+    }
     
+    
+if(devicepurchase){
+        let sType = "Compra";
+        const oHeadersGet = { 'Authorization': "Basic c2ItY2xvbmUtZDhjMmJmNDgtYmNmZS00NzVhLThjN2MtZjZiZGU4YzI1MWRjIWIxMjYzM3x3b3JrZmxvdyFiMTc3NDo0N2RiODJkMS02OTM1LTQ0OTctYTkxZS1lNTFhNDVmMDExYjQkNGFsMzVBVkFrWnZLa2hzdE5kUWR5ZERWR0ptSkNHX0hKYWlXcXJGQlBmRT0=" }  // Basic Authorization in base 64, clientid is user and cliendsecret is the password
+        const oParamsGet = {
+            json: true,
+            headers: oHeadersGet
+        }
+         let resGet = request('POST', 'https://c9de1e9btrial.authentication.us10.hana.ondemand.com/oauth/token?grant_type=client_credentials', oParamsGet);
+         let bodyGet = JSON.parse(resGet.getBody('utf8'));
+        //console.log("bodyGet", bodyGet.access_token)
+        const oHeadersPost = { 
+             'Authorization': 'Bearer '+ bodyGet.access_token,
+             'Content-Type':'application/json'
+              }
+         let oContext = {
+                 "definitionId": "testWorkflow",
+                 "context": {
+                     "sType": sType,
+                     "users":oUser,
+                     "items":aContext
+                         }
+                     }
+        //const oHeadersPost = { 'Authorization': "Basic c2ItY2xvbmUtZDhjMmJmNDgtYmNmZS00NzVhLThjN2MtZjZiZGU4YzI1MWRjIWIxMjYzM3x3b3JrZmxvdyFiMTc3NDo0N2RiODJkMS02OTM1LTQ0OTctYTkxZS1lNTFhNDVmMDExYjQkNGFsMzVBVkFrWnZLa2hzdE5kUWR5ZERWR0ptSkNHX0hKYWlXcXJGQlBmRT0=" }  // Basic Authorization in base 64, clientid is user and cliendsecret is the password
+        const oParamsPost = {
+            json: oContext,
+            headers: oHeadersPost
+        }
+
+        try{
+         let resPost = request('POST', 'https://api.workflow-sap.cfapps.us10.hana.ondemand.com/workflow-service/rest/v1/workflow-instances', oParamsPost);
+         let bodyPost =JSON.parse(resPost.getBody('utf8'));
+         console.log("bodyPost ", bodyPost);
+        if(bodyPost.status==='RUNNING'){
+            data.replies.push({
+                "type": "text",
+                "content": "Se activó el flujo de aprobación para tu solicitud de compra con éxito."
+            })
+        }else{
+            data.replies.push({
+                "type": "text",
+                "content": "Ha surgido un error al momento de activar el flujo de aprobación, por favor intente más tarde."
+            });
+            sBolean = false;
+        }
+ 
+        }catch(error){
+            data.replies.push({
+                "type": "text",
+                "content": "Ha surgido un error al momento de activar el flujo de aprobación, por favor intente más tarde."
+            }); 
+            sBolean = false;
+         
+        }
+        
+        
+     }
+     
+
+
+    if(sBolean){
+      data.replies.push({
+            "type": "buttons",
+            "content": {
+            "title": "Para evaluar tu experiencia usando, nos gustaría que contestarás la siguiente encuesta.",
+            "buttons": [{
+                 "title": "Ir a encuesta",
+                 "type": "web_url",
+                 "value": "https://qfreeaccountssjc1.az1.qualtrics.com/jfe/form/SV_0Mt7xvqiuPtqQoR"
+                }]
+            }
+        })
+    }
+
+     res.send(data);
 
 
 });
 
 
-function triggerInstances(aContext){
-    const oHeaders = { 'Authorization': "Basic c2ItY2xvbmUtZDhjMmJmNDgtYmNmZS00NzVhLThjN2MtZjZiZGU4YzI1MWRjIWIxMjYzM3x3b3JrZmxvdyFiMTc3NDo0N2RiODJkMS02OTM1LTQ0OTctYTkxZS1lNTFhNDVmMDExYjQkNGFsMzVBVkFrWnZLa2hzdE5kUWR5ZERWR0ptSkNHX0hKYWlXcXJGQlBmRT0=" }  // Basic Authorization in base 64, clientid is user and cliendsecret is the password
 
-    const oParams = {
-        method: 'POST',
-        json: true,
-        url: 'https://c9de1e9btrial.authentication.us10.hana.ondemand.com/oauth/token?grant_type=client_credentials',
-        headers: oHeaders
-    }
-    request(oParams, function(error, response, body){
-        let token = response.body.access_token;
-        const oHeaders1 = { 
-            'Authorization': 'Bearer '+token,
-            'Content-Type':'application/json'
-         }  
-     
-         console.log("oheaders "+ JSON.stringify(oHeaders1))
-         let oContext = {
-             "definitionId": "testWorkflow"
-             //"context": {
-             //    "items":aContext
-             //}
-         }
-         console.log(oContext)
-         const oParams1 = {
-             method: 'POST',
-             json: oContext,
-             url: 'https://api.workflow-sap.cfapps.us10.hana.ondemand.com/workflow-service/rest/v1/workflow-instances',
-             headers: oHeaders1
-         }
-         function callback(error, response, body) {
-             if (!error) {
-                 console.log("success " +JSON.stringify(response))
-             } else {
-                 console.log('error' + error);
-             }
-         }
-         request(oParams1, callback);
-     
-
-    });
-    
-
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
